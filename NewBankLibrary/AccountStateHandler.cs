0f0504a5d627d69f927875e44b6fcbd93717778c
SergeiPodobed <izotep19@yandex.ru>
2020-05-10 14:14:39 +0300

    Add project files.

---
+++ NewBankLibrary/AccountStateHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewBankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+    public class AccountEventArgs
+    {
+        // Сообщение
+        public string Message { get; set; }
+        // Сумма, на которую изменился счет // число купленных-сданных билетов
+        public decimal Sum { get; set; }
+
+        public AccountEventArgs(string _mes, decimal _sum)
+        {
+            Message = _mes;
+            Sum = _sum;
+        }
+    }
+}
---
+++ NewBankLibrary/IAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewBankLibrary
+{
+    public interface IAccount
+    {
+        // Купить билет
+        void Put(decimal sum);
+        // Сдать билет
+        decimal Withdraw(decimal sum);
+    }
+}
